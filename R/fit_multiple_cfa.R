c950dfdc544ad0d737df5fc579c61df5abd29a6e
Marclianjie <lianjieshi02@gmail.com>
2024-08-06 11:16:15 -0700

    finishi function fit_multiple_cfa

---
+++ R/fit_multiple_cfa.R
@@ -0,0 +1,18 @@
+
+fit_multiple_cfa <- function(models, data, ...) {
+  if (!is.character(models)) {
+    stop("Models argument should be a character vector of model specifications.")
+  }
+
+  fit_model <- function(model) {
+    fit <- lavaan::cfa(model, data = data, ...)
+    return(fit)
+  }
+
+  results <- purrr::map(models, fit_model)
+  names(results) <- paste0("Model_", seq_along(models))
+  summaries <- purrr::map(results, lavaan::summary)
+
+  return(summaries)
+}
+
